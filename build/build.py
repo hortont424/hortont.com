d44abcc320734ade088d6fed18808c143698eae3
Tim Horton <hortont424@gmail.com>
2010-06-26 02:02:56 -0700

    fix old-old urls

--- build/build.py
+++ build/build.py
@@ -22,10 +22,13 @@ def buildBackwardsCompatibilityLinks(type):
             if not f.endswith(".control"): # use generatePostList
                 continue
 
+            oldStyle = False
             metadata = json.loads(readFileContents(f), encoding='utf-8')
 
             if "post-name" not in metadata:
                 metadata["post-name"] = generatePostName(metadata["title"])
+            else:
+                oldStyle = True
 
             if metadata["post-name"] in seenNames:
                 print "\n\nDuplicate post-name: ", metadata["post-name"], "\n\n"
@@ -45,6 +48,18 @@ def buildBackwardsCompatibilityLinks(type):
             out = codecs.open(outputFile, mode="a")
             out.write(htaccessContents)
             out.close()
+
+            if oldStyle:
+                ourDate = datetime.datetime.strptime(metadata["date"], "%Y.%m.%d %H:%M:%S").strftime("%Y/%m") # TODO: fixed slash
+                outputFolder = os.path.join("output", blog_dir, ourDate, metadata["post-name"])
+                if not os.path.exists(outputFolder):
+                    os.makedirs(outputFolder)
+                outputFile = os.path.join(outputFolder, ".htaccess")
+                realURL = os.path.join(blog_prefix, "posts", f.replace(".control", ".html").replace("posts" + "/", ""))
+                htaccessContents = "RewriteEngine on\n" + "RewriteRule ^.*$ " + realURL + "\n"
+                out = codecs.open(outputFile, mode="w+")
+                out.write(htaccessContents)
+                out.close()
 
 def generateCategoryMap(type):
     posts = {}
